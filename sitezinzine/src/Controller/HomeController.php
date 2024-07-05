0b9084b7e7b0d7ecb8ff73d13531b76d60331873
EloiseMcGlasson <mc.glasson@free.fr>
2024-07-05 16:42:00 +0200

    finalisation login et registration + verification email, redirection apres confirmation email ne marche pas

--- sitezinzine/src/Controller/HomeController.php
+++ sitezinzine/src/Controller/HomeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
@@ -12,6 +16,7 @@ class HomeController extends AbstractController
     #[Route("/", name: "home")]
     function index(Request $request): Response
     {
+        
         return $this->render('home/index.html.twig');
     }
 }
