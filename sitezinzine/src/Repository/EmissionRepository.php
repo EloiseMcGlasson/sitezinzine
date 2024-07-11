19f2ed7e3436e15223723ff2b9e908e16779ff70
EloiseMcGlasson <mc.glasson@free.fr>
2024-07-11 10:38:06 +0200

    pagination emissions et categories, count nbemissions par categories

--- sitezinzine/src/Repository/EmissionRepository.php
+++ sitezinzine/src/Repository/EmissionRepository.php
@@ -5,17 +5,53 @@ namespace App\Repository;
 use App\Entity\Emission;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @extends ServiceEntityRepository<Emission>
  */
 class EmissionRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(ManagerRegistry $registry, private PaginatorInterface $paginator)
     {
         parent::__construct($registry, Emission::class);
     }
 
+    public function paginateEmissions(int $page, $value): PaginationInterface
+    {
+        
+        return $this->paginator->paginate(
+            
+            $this->createQueryBuilder('r')
+            ->select('r', 'c')
+            ->leftJoin('r.categorie', 'c') 
+            ->andWhere('r.url != :val')
+            ->setParameter('val', $value)
+            ->orderBy('r.datepub', 'DESC'),
+            $page,
+            25,
+            [
+                'distinct' => true,
+                'sortFieldAllowList' => ['r.titre']
+            ]
+        );
+        /* return new Paginator($this
+            ->createQueryBuilder('r')
+            ->setFirstResult(($page - 1) * $limit)
+            ->setMaxResults($limit)
+            ->select('r', 'c')
+            ->andWhere('r.url != :val')
+            ->setParameter('val', $value)
+            ->orderBy('r.datepub', 'DESC')
+            ->leftJoin('r.categorie', 'c')
+            ->getQuery()
+            ->setHint(Paginator::HINT_ENABLE_DISTINCT, false)
+            
+        ); */
+            
+    }
     //    /**
     //     * @return Emission[] Returns an array of Emission objects
     //     */
