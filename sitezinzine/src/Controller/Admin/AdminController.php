5568f77e1821a935fd359d8ff7682b0db5b6253b
EloiseMcGlasson <mc.glasson@free.fr>
2025-03-18 18:31:57 +0100

    création page d'accueil admin avec personnalisation du nom utilisateur

---
+++ sitezinzine/src/Controller/Admin/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route("/admin", name: 'admin.')]
+#[IsGranted('ROLE_USER')]
+class AdminController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        $user = $this->getUser();
+    return $this->render('admin/index.html.twig', [
+        'user' => $user
+        ]);
+    }
+}
