e01104de7df4db462d5fbab28b19f17c205d833b
EloiseMcGlasson <mc.glasson@free.fr>
2025-08-03 16:15:33 +0200

    correction des liens semaine précédente et suivante

--- sitezinzine/src/Controller/Admin/GrilleController.php
+++ sitezinzine/src/Controller/Admin/GrilleController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Component\HttpFoundation\Request;
 
 #[Route("/admin/grille", name: 'admin.grille.')]
 #[IsGranted("ROLE_ADMIN")]
@@ -14,20 +13,20 @@ use Symfony\Component\HttpFoundation\Request;
 class GrilleController extends AbstractController
 {
 
-    #[Route('/', name: 'index')]
-    public function index(): Response
-    {
-        // On prend aujourd'hui
-        // Dans le contrôleur
-        $today = new \DateTimeImmutable();
-        
-        
-        // On calcule le mardi de la semaine en cours
-        $startOfWeek = $today->modify('this week')->modify('+1 day'); // Lundi + 1 => Mardi
-        $startOfWeek = $startOfWeek->setTime(0, 0); // On met l'heure à 00:00
+    #[Route('/{startOfWeek?}', name: 'index')]
+public function index(?string $startOfWeek): Response
+{
+    // Si une date est passée, on l'utilise, sinon on prend aujourd'hui
+    $startDate = $startOfWeek
+        ? \DateTime::createFromFormat('Y-m-d', $startOfWeek)
+        : new \DateTime();
 
-        return $this->render('admin/grille/index.html.twig', [
-            'startOfWeek' => $startOfWeek,
-        ]);
-    }
+    // On aligne sur le mardi de la semaine
+    $startOfWeek = $startDate->modify('this week')->modify('+1 day')->setTime(0, 0);
+
+    return $this->render('admin/grille/index.html.twig', [
+        'startOfWeek' => $startOfWeek,
+    ]);
+}
+
 }
