01ab07ecc0ddcc99a2d5338b078f939882890a2a
EloiseMcGlasson <mc.glasson@free.fr>
2026-03-25 16:16:36 +0100

    Prise en charge de l'écriture des tags sur les fichiers Mp3 uploadés (sauf imageTags).

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '25ca155f244bbc7b1147c401a9ba6c9045b414c3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '25ca155f244bbc7b1147c401a9ba6c9045b414c3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'james-heinrich/getid3' => array(
+            'pretty_version' => 'v1.9.25',
+            'version' => '1.9.25.0',
+            'reference' => 'fefffe762b02be155dcc32eec57feff8a49bc4b5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../james-heinrich/getid3',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
